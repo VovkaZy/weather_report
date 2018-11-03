c4e2d7423699bf03a391bf8f9e8494ae1b9a9985
VOLODYMYR <v.n.zubarev@gmail.com>
2018-11-03 21:04:59 +0200

    comments added

--- weather.rb
+++ weather.rb
@@ -1,9 +1,15 @@
+# Weather_report is a console app for showing weather using XML parsing
+# ver.: 0.9
+# website: https://www.meteoservice.ru/content/export
+# Created by v.n.zubarev@gmail.com on 03/11/18
 
 require 'net/http'
 require 'uri'
 require 'rexml/document'
 
+# Hash with cloudiness variations
 CLOUDINESS = { 0 => 'Clear', 1 => 'Partly cloudy', 2 => 'Cloudy', 3 => 'Overcast'}
+
 uri = URI.parse('https://xml.meteoservice.ru/export/gismeteo/point/25.xml')
 
 response = Net::HTTP.get_response(uri)
@@ -14,14 +20,15 @@ city_name = URI.unescape(doc.root.elements['REPORT/TOWN'].attributes['sname'])
 
 current_forcast = doc.root.elements['REPORT/TOWN'].elements.to_a[0]
 
-# temperature
+# temperature min and max
 min_temp = current_forcast.elements['TEMPERATURE'].attributes['min']
 max_temp = current_forcast.elements['TEMPERATURE'].attributes['max']
 
+# wind speed
 max_wind = current_forcast.elements['WIND'].attributes['max']
 
+# cloudiness
 cloud_index = current_forcast.elements['PHENOMENA'].attributes['cloudiness'].to_i
-
 clouds = CLOUDINESS[cloud_index]
 
 puts '|-----------------------------|'
